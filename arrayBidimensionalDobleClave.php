3a6ca54472362b9412e7feaa6775f9f56cb0ac41
ANGELMUSEO <angel.gonzalezfdz@gmail.com>
2024-01-16 12:08:04 +0100

    ARRAY BIDIMENSIONALES CLAVES

--- arrayBidimensionalDobleClave.php
+++ arrayBidimensionalDobleClave.php
@@ -9,7 +9,7 @@
     
 <?php
         $temperaturasCiudad=[
-            "OVIEDO"=>["mañana"=>3,
+            "OVIEDO"=>["mañana"=>"hlk",
                         "tarde"=>15, 
                         "noche"=>12],
             "GIJON"=>["mañana"=>6,
@@ -22,12 +22,31 @@
 
        // echo $temperaturasCiudad["GIJON"]["tarde"];
 
+     /*
         foreach($temperaturasCiudad as $nombre=>$temperatura){
             echo $nombre."<br>";
             echo "TEMPERATURA MAÑANA: {$temperatura["mañana"]} TEMPERATURA TARDE:{$temperatura["tarde"]} TEMPERATURA NOCHE {$temperatura["noche"]}<br><br>";
         }
-
+*/
         //foreach doble
+
+        /*
+        foreach($temperaturasCiudad as $nombre=>$temperaturas){
+            foreach($temperaturas as $momento=>$valor){
+                    echo "{$nombre}--{$momento}--{$valor} <br>";
+                
+            }
+           
+         }
+
+        echo"<br><br><br>La media es ".($totalTemperaturas/12);
+*/
+foreach($temperaturasCiudad as $nombre=>$temperaturas){
+    foreach($temperaturas as $momento=>$valor){
+        if(is_numeric($valor)){
+            echo $valor."<br>";
+        }
+    }}
 
 ?>
 </body>
---
+++ zara.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*Necesitamos gestionar una tienda de ropa teniendo en cuenta que cada producto tiene:
+    codigoBarras, nombreProducto, precio,color, talla. Meter 3 productos */
+$tienda=[
+        "camisa"=>["nombre"=>"camisa","precio"=>32,"color"=>"rojo","talla"=>"xl"],
+        2345=>[],
+        1973=>[]
+];
+
+
+    ?>
+    
+</body>
+</html>
