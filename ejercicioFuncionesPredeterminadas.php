8ae6bce77f8f6c7158f31c8d39894b2d4db4d946
ANGELMUSEO <angel.gonzalezfdz@gmail.com>
2024-01-19 10:54:00 +0100

    datosCreados por Manolo

---
+++ ejercicioFuncionesPredeterminadas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*Crea un programa en PHP que utilice diversas funciones para manipular un array
+     de strings que representa una lista de tareas. Implementa las siguientes operaciones:
+        Implementa cada operación como una función separada y muestra los resultados utilizando un bucle foreach.
+-Añadir Tarea al Final
+-Añadir Tarea al Principio
+-Eliminar Última Tarea
+-Eliminar Primera Tarea
+-Actualizar Tarea Específica
+-Crear una Cadena de Tareas
+-Dividir Cadena en un Array
+-Crear un array con claves y valores. Por ejemplo ciudades y habitantes
+-Obtener Claves del Array
+-Obtener Valores del Array
+-Combinar Dos Arrays en uno
+-Verificar Existencia de Tarea
+-Contar el Número de Tareas
+
+*/
+    ?>
+</body>
+</html>
